87d38a8d661eeae7878d1006a8311852e50ea296
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-20 22:17:41 +0530

    Add ServiceCard component and update Services layout
    Introduced a new ServiceCard component for displaying individual service details with interactive effects. Updated Services.jsx to use ServiceCard for rendering service items in a grid layout. Minor adjustments made to custom variant comments in index.css.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
     const servicesData = [
@@ -29,7 +30,13 @@ const Services = () => {
         <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
             <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden' />
 
-            <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward.' /    >
+            <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
+
+            <div className='flex flex-col md:grid grid-cols-2'>
+                {servicesData.map((service, index) => (
+                    <ServiceCard key={index} service={service} index={index} />
+                ))}
+            </div>
 
         </div>
     )
