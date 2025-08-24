035d4067d8bad90208de6a6ccb28635842cff189
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-24 22:56:13 +0530

    Add dynamic work cards to OurWork component
    Introduced a workData array and mapped over it to render work cards dynamically in the OurWork component. This improves maintainability and scalability by centralizing work item data and removing hardcoded content.

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import Title from './Title'
+import assets from '../assets/assets'
 
 const OurWork = () => {
+
+    const workData = [
+        {
+            title: 'Mobile app marketing',
+            desc: 'We turn bold ideas into powerful digital solutions that connect, engage',
+            imgage: assets.work_mobile_app
+        },
+        {
+            title: 'Dashboard management',
+            desc: 'We help you execute your plan and deliver results..',
+            imgage: assets.work_dashboard_management
+        },
+        {
+            title: 'Fitness app promotion',
+            desc: 'We help you create a marketing strategy that drives results.',
+            imgage: assets.work_fitness_app
+        },
+    ]
+
     return (
         <div id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 pt-30 dark:text-white'>
             <Title title='Our Latest Work' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
+
+            <div>
+                <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl'>
+                    {workData.map((work, index) => (
+                        <div key={index} className='hover:scale-102 duration-500 transition-all cursor-pointer'>
+                            <img src={work.imgage} alt={work.title} className='w-full rounded-xl' />
+                            <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
+                            <p className='text-sm opacity-60 w-5/6'>{work.desc}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
