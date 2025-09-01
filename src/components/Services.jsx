6dc20f90cdadeafa67cbaf3a1b5a24eac9ead43c
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-01 23:51:39 +0530

    Add motion animations to service and trusted components
    Integrated Framer Motion animations into ServiceCard, Services, Title, and TrustedBy components for improved UI transitions and visual effects. This enhances user experience by animating component entry and staggered child elements.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
 import ServiceCard from './ServiceCard'
-
+import { motion } from 'motion/react'
 const Services = () => {
     const servicesData = [
         {
@@ -27,7 +27,13 @@ const Services = () => {
         }
     ]
     return (
-        <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <motion.div
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
+            transition={{ staggerChildren: 0.2 }}
+
+            id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
             <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden' />
 
             <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
@@ -38,7 +44,7 @@ const Services = () => {
                 ))}
             </div>
 
-        </div>
+        </motion.div>
     )
 }
 
