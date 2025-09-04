bec1667ef92940d4bf61392eacf22660c06b626f
Sahil Zalte <zaltesahil94@gmail.com>
2025-09-04 21:16:07 +0530

    Add motion animations to ContactUs, OurWork, Teams
    Integrated Framer Motion to ContactUs, OurWork, and Teams components for improved UI animations. Elements now animate into view with staggered transitions, enhancing user experience and visual appeal.

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast';
+import { motion } from 'motion/react'
 
 const ContactUs = () => {
     const onSubmit = async (event) => {
@@ -31,9 +32,18 @@ const ContactUs = () => {
 
     };
     return (
-        <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <motion.div
+            initial='hidden'
+            viewport={{ once: true }}
+            transition={{ staggerChildren: 0.2 }}
+            id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
             <Title title='Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
-            <form onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+            <motion.form
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+                viewport={{ once: true }}
+                onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
                 <div>
                     <p className='mb-2 text-sm font-medium'>Your name</p>
                     <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
@@ -57,8 +67,8 @@ const ContactUs = () => {
                 <button type='submit' className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all'>
                     Submit <img src={assets.arrow_icon} alt="" className='w-4' />
                 </button>
-            </form>
-        </div>
+            </motion.form>
+        </motion.div>
     )
 }
 
